c6d33c4b17d714d485f15f8ed64fac0aa87422b0
Georgios Petrousis <georgios.petrousis@nentgroup.com>
2019-12-02 17:54:01 +0100

    Add readFile package to read the input file for each challenge

---
+++ readFile/readFile.go
@@ -0,0 +1,22 @@
+package readFile
+
+import (
+	"os"
+	"fmt"
+	"bufio"
+)
+
+func ReadFile(input string) ([]string, error) {
+	file, err := os.Open(input)
+	if err != nil {
+        fmt.Println("File reading error", err)
+        return nil, err 
+	}
+	defer file.Close()
+	var lines []string
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+        lines = append(lines, scanner.Text())
+    }
+    return lines, scanner.Err()
+}
