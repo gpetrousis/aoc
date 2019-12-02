841280aab13d9cf5a13c0a6f80397a89046f77fd
Georgios Petrousis <georgios.petrousis@nentgroup.com>
2019-12-02 20:07:57 +0100

    Refactor and add Day1 Part2

--- day1/day1.go
+++ day1/day1.go
@@ -3,31 +3,31 @@ package main
 import (
 	"fmt"
 	"strconv"
-
-	"github.com/gpetrousis/aoc2019/readFile"
+	"github.com/gpetrousis/aoc2019/readfile"
 )
 
-func calculateFuelConsumption(mass int) int {
-	return mass/3 - 2
-}
 
-func calculateTotalFuelConsumption(masses []string) int {
-	var totalConspumption int = 0
+func main() {
+	masses, err := readfile.ReadFile("input.txt")
+	if err != nil {
+		fmt.Println("File reading error", err)
+	}
+
+	var totalConsumption int = 0
+	var totalConsumptionWithExtra int = 0
 	for _, mass := range masses {
 		intMass, err := strconv.Atoi(mass)
 		if err != nil {
 			fmt.Println("Cannot convert string to int", err)
 		}
-		totalConspumption += calculateFuelConsumption(intMass)
+		// Not the most optimal solution. Just for the sake of two parts for the
+		// challenge
+		totalConsumption += calculateConsumption(intMass)
+		totalConsumptionWithExtra += calculateConsumptionWithExtra(intMass)
 	}
-	return totalConspumption
-}
 
-func main() {
-	masses, err := readFile.ReadFile("input.txt")
-	if err != nil {
-		fmt.Println("File reading error", err)
-	}
-	totalConspumption := calculateTotalFuelConsumption(masses)
-	fmt.Println("Total fuel consumption:", totalConspumption)
+	fmt.Println("[Part1] Total fuel consumption:", totalConsumption)
+
+	// totalConspumption := sumConsumption + calculateExtraConsumption(sumConsumption)
+	// fmt.Println("Total fuel consumption:", totalConspumption)
 }
--- readFile/readFile.go
+++ readFile/readFile.go
@@ -1,4 +1,4 @@
-package readFile
+package readfile
 
 import (
 	"os"
