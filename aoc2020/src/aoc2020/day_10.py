69e414abfb3b2829ad90e6c6b73cc1afe5e912cb
Georgios Petrousis <gpetrousis@gmail.com>
2020-12-13 22:24:14 +0100

    AOC2020: Add Day 10

---
+++ aoc2020/src/aoc2020/day_10.py
@@ -0,0 +1,62 @@
+""" AOC Day 10 """
+import os
+import click
+
+
+def parse_input(input_path):
+    """Read the input file and parse it into an array of integers"""
+    numbers = []
+    with open(os.path.abspath(input_path)) as input_file:
+        for line in input_file:
+            numbers.append(int(line.rstrip("\n")))
+
+    return numbers
+
+
+def get_joltage_difference(jolts):
+    """Return the product of the 1-jolt and 3-jolt differences"""
+    sorted_jolts = sorted(jolts)
+    sorted_jolts.insert(0, 0)  # Add the first jolt of the wall plug
+    sorted_jolts.append(sorted_jolts[-1] + 3)  # Add the last jolt of the laptop plug
+
+    diffs = [0, 0, 0]
+    for i in range(0, len(sorted_jolts) - 1):
+        jolt_diff = sorted_jolts[i + 1] - sorted_jolts[i] - 1
+
+        if jolt_diff > 2:
+            click.echo("Jolt difference too big")
+            return None
+
+        diffs[jolt_diff] = diffs[jolt_diff] + 1
+
+    return diffs[0] * diffs[2]
+
+
+def get_joltage_arrangements(jolts):
+    """Return the number of distinct arrangements"""
+    sorted_jolts = sorted(jolts)
+    sorted_jolts.append(sorted_jolts[-1] + 3)  # Add the last jolt of the laptop plug
+
+    memoization = {0: 1}
+
+    for jolt in sorted_jolts:
+        memoization[jolt] = (
+            memoization.get(jolt - 3, 0)
+            + memoization.get(jolt - 2, 0)
+            + memoization.get(jolt - 1, 0)
+        )
+
+    return memoization[sorted_jolts[-1]]
+
+
+@click.command()
+@click.argument("input_path", type=click.Path(exists=True))
+def main(input_path):
+    """ AOC Day 10 Main """
+    jolts = parse_input(input_path)
+    click.echo(get_joltage_difference(jolts))
+    click.echo(get_joltage_arrangements(jolts))
+
+
+if __name__ == "__main__":
+    main()
---
+++ aoc2020/tests/test_day_10.py
@@ -0,0 +1,68 @@
+""" Day 10 Unit test """
+
+from aoc2020 import day_10
+
+parsed_input = [
+    28,
+    33,
+    18,
+    42,
+    31,
+    14,
+    46,
+    20,
+    48,
+    47,
+    24,
+    23,
+    49,
+    45,
+    19,
+    38,
+    39,
+    11,
+    1,
+    32,
+    25,
+    35,
+    8,
+    17,
+    7,
+    9,
+    4,
+    2,
+    34,
+    10,
+    3,
+]
+
+small_input = [
+    16,
+    10,
+    15,
+    5,
+    1,
+    11,
+    7,
+    19,
+    6,
+    12,
+    4,
+]
+
+
+def test_parse_input():
+    """ Test parse_input function """
+    assert day_10.parse_input("./data/day10/test_input.txt") == parsed_input
+
+
+def test_get_joltage_difference():
+    """ Test get_joltage_difference function """
+    assert day_10.get_joltage_difference(small_input) == 35
+    assert day_10.get_joltage_difference(parsed_input) == 220
+
+
+def test_get_joltage_arrangements():
+    """ Test get_joltage_arrangements function """
+    assert day_10.get_joltage_arrangements(small_input) == 8
+    assert day_10.get_joltage_arrangements(parsed_input) == 19208
